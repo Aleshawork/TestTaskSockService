5eca254c95059dd0f1d9e8ef5edd69310acd2da9
Alexey <borisov_alexey_2002@mail.ru>
2021-10-09 16:20:15 +0300

    add api swagger documentation

--- src/main/java/com/example/socksservice/controller/SockController.java
+++ src/main/java/com/example/socksservice/controller/SockController.java
@@ -2,6 +2,9 @@ package com.example.socksservice.controller;
 
 import com.example.socksservice.model.Sock;
 import com.example.socksservice.service.SockService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,19 +20,38 @@ public class SockController {
         this.sockService = sockService;
     }
 
+    @ApiOperation(value = "Registers the arrival of socks at the warehouse")
+    @ApiResponses(value={
+            @ApiResponse(code = 200, message ="Managed to add an income"),
+            @ApiResponse(code=400, message="The request parameters are missing or have an incorrect format"),
+            @ApiResponse(code=500, message ="Error on the server")
+    }
+    )
     @PostMapping("/income")
     public HttpStatus income(@RequestBody Sock sock){
         sockService.income(sock);
         return  HttpStatus.OK;
     }
 
+
+    @ApiOperation(value = "Registers the release of socks from the warehouse")
+    @ApiResponses(value={
+            @ApiResponse(code = 200, message ="Managed to add an outcome"),
+            @ApiResponse(code=400, message="The request parameters are missing or have an incorrect format"),
+            @ApiResponse(code=500, message ="Error on the server")
+    })
     @PostMapping("/outcome")
     public HttpStatus outcome(@RequestBody Sock sock){
         sockService.outcome(sock);
         return HttpStatus.OK;
     }
 
-    ///{operation}/{cottonPart}
+    @ApiOperation(value = "Returns the total number of socks in stock that match the request criteria passed in the parameters")
+    @ApiResponses(value={
+            @ApiResponse(code = 200, message ="The request is executed, the result is in the response body as a string representation of an integer"),
+            @ApiResponse(code=400, message="The request parameters are missing or have an incorrect format"),
+            @ApiResponse(code=500, message ="Error on the server")
+    })
     @GetMapping()
     public int findCount(@RequestParam("color")String color,
                          @RequestParam("operation")String operation,
